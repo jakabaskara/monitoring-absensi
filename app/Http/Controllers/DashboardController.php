e2e55cdb81e468176b7731ddad8fc57f75825420
selvi <gregoriamarianaselvi@gmail.com>
2024-03-06 14:47:44 +0700

    coding API

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class DashboardController extends Controller
 {
     public function dashboard()
     {
-        return view('dashboard');
+        $response = Http::get('https://api.fingerspot.io/api/download/attendance_log/C262B8950323192C/2024-02-06/6/emp_nik/desc/json/8f2cf595e66939f6cecc14ec3388926d/20240207040900');
+        $absensi = $response->json();
+        return view('dashboard', ['absensi' => $absensi]);
     }
 }
